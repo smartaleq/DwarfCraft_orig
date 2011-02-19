95046a5e5ec9c273aecfe45ee14ba6fda7955ae7
RCarretta <RCarretta@gmail.com>
2011-02-19 00:24:58 -0800

    formatting in ConfigManager

--- src/com/smartaleq/bukkit/dwarfcraft/ConfigManager.java
+++ src/com/smartaleq/bukkit/dwarfcraft/ConfigManager.java
@@ -87,25 +87,25 @@ public class ConfigManager {
 					continue;
 					}
 				
-				int effectId = Integer.parseInt(theline[0]); 
-				double baseValue = Double.parseDouble(theline[1]);
-				double levelUpMultiplier = Double.parseDouble(theline[2]);
-				double noviceLevelUpMultiplier = Double.parseDouble(theline[3]);
-				double minValue = Double.parseDouble(theline[4]);
-				double maxValue = Double.parseDouble(theline[5]);
-				boolean floorResult = (theline[6] == "1"); 
-				boolean hasException = (theline[7] == "1"); 
-				int exceptionLow = Integer.parseInt(theline[8]); 
-				int exceptionHigh = Integer.parseInt(theline[9]); 
-				double exceptionValue = Double.parseDouble(theline[10]);
-				int elfLevel = Integer.parseInt(theline[11]);
-				EffectType effectType = EffectType.getEffectType(theline[12]);
-				int initiator = Integer.parseInt(theline[13]); 
-				int output = Integer.parseInt(theline[14]); 
-				boolean allowFist = (theline[15] == "TRUE");
+				int			effectId 				= Integer.parseInt(theline[0]); 
+				double 		baseValue 				= Double.parseDouble(theline[1]);
+				double 		levelUpMultiplier 		= Double.parseDouble(theline[2]);
+				double 		noviceLevelUpMultiplier = Double.parseDouble(theline[3]);
+				double 		minValue 				= Double.parseDouble(theline[4]);
+				double 		maxValue 				= Double.parseDouble(theline[5]);
+				boolean 	floorResult 			= (theline[6] == "1"); 
+				boolean 	hasException 			= (theline[7] == "1"); 
+				int 		exceptionLow 			= Integer.parseInt(theline[8]); 
+				int 		exceptionHigh 			= Integer.parseInt(theline[9]); 
+				double 		exceptionValue 			= Double.parseDouble(theline[10]);
+				int 		elfLevel 				= Integer.parseInt(theline[11]);
+				EffectType 	effectType 				= EffectType.getEffectType(theline[12]);
+				int 		initiator 				= Integer.parseInt(theline[13]); 
+				int 		output 					= Integer.parseInt(theline[14]); 
+				boolean 	allowFist 				= (theline[15] == "TRUE");
 
 				int[] tooltable = 
-						{Integer.parseInt(theline[16]),
+					{	Integer.parseInt(theline[16]),
 						Integer.parseInt(theline[17]),
 						Integer.parseInt(theline[18]),
 						Integer.parseInt(theline[19]),
